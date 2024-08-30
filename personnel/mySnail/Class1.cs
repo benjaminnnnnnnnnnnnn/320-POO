ccfae5aec493bc1f636aeb20f4515a4f236a2b21
benjaminnnnnnnnnnnnn <px25twk@eduvaud.ch>
2024-08-30 11:44:10 +0200

    terminé l'exercice snail

---
+++ personnel/mySnail/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExSnail
+{
+    internal class Snail
+    {
+
+        //atribut 
+
+        public int vie = 50;             //vie du escargot
+        public int x = 0;                //position
+        public int y;                   //possition
+        public string alive = "_@_ö";    //escargot vivant
+        public string dead = "____";     //escargot mort
+        public string name;
+
+        //constructeur
+
+        public Snail(int y)
+        {
+            this.name = "esgargot" + y;
+            this.y = y;
+        }
+
+        //metode
+
+        public void move()
+        {
+            x++;
+            vie--;
+        }
+    }
+}
--- personnel/mySnail/Program.cs
+++ personnel/mySnail/Program.cs
@@ -1,28 +1,67 @@
-﻿namespace mysnail
+﻿using ExSnail;
+
+namespace mysnail
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("_@_ö");
 
-            int vie = 0;
+            List<Snail> snails = new List<Snail>();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                snails.Add(new Snail(i));
+
+
+            }
+
+
 
             Console.CursorVisible = false;
-
-            while (vie != 50)
+            Console.ForegroundColor = ConsoleColor.Green;
+            while (snails[0].vie > 0)
             {
 
                 Console.Clear();
-                Console.Write(50 - vie);
-                Console.SetCursorPosition(vie, 0);
-                Console.Write("_@_ö");
-                vie++;
-                Thread.Sleep(100);
+
+                foreach (Snail snail in snails)
+                {
+                    if (snail.vie == 30)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    if (snail.vie == 10)
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write(snail.vie);
+
+
+                    Console.SetCursorPosition(snail.x, snail.y);
+                    Console.Write(snail.alive);
+
+
+                    snail.move();
+
+
+                }
+                Thread.Sleep(40);
+
             }
+
             Console.Clear();
-            Console.SetCursorPosition(vie, 0);
-            Console.Write("___");
+            foreach (Snail snail in snails)
+            {
+                Console.SetCursorPosition(snail.x, snail.y);
+                Console.Write(snail.dead);
+
+
+
+
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
