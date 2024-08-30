745e7949730d17c0e1089f698d07cde99f161ede
benjaminnnnnnnnnnnnn <px25twk@eduvaud.ch>
2024-08-30 10:20:34 +0200

    add snail :)

---
+++ personnel/mySnail/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace mysnail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("_@_ö");
+
+            int vie = 0;
+
+            Console.CursorVisible = false;
+
+            while (vie != 50)
+            {
+
+                Console.Clear();
+                Console.Write(50 - vie);
+                Console.SetCursorPosition(vie, 0);
+                Console.Write("_@_ö");
+                vie++;
+                Thread.Sleep(100);
+            }
+            Console.Clear();
+            Console.SetCursorPosition(vie, 0);
+            Console.Write("___");
+        }
+    }
+}
