0be257757ad45f0019fccb6a91b75d54e73dd59f
benjaminnnnnnnnnnnnn <px25twk@eduvaud.ch>
2024-09-11 13:52:53 +0200

    disgin fish

--- personnel/Drones/Drones/View/Drone.cs
+++ personnel/Drones/Drones/View/Drone.cs
@@ -6,13 +6,16 @@ namespace Drones
 
     public partial class Drone
     {
-        private Pen droneBrush = new Pen(new SolidBrush(Color.Purple), 3);
+        private Pen droneBrush = new Pen(new SolidBrush(Color.DeepSkyBlue), 3);
+        private Pen droneBrushblack = new Pen(new SolidBrush(Color.Black), 1);
 
         // De manière graphique
         public void Render(BufferedGraphics drawingSpace)
         {
-            drawingSpace.Graphics.DrawEllipse(droneBrush, new Rectangle(X - 4, Y - 2, 8, 8));
-            drawingSpace.Graphics.DrawString($"{this}", TextHelpers.drawFont, TextHelpers.writingBrush, X + 5, Y - 5);
+            drawingSpace.Graphics.DrawEllipse(droneBrush, new Rectangle(X - 6, Y - 2, 15, 8));
+            drawingSpace.Graphics.DrawEllipse(droneBrush, new Rectangle(X +10, Y - 2, 5, 8));
+            drawingSpace.Graphics.DrawEllipse(droneBrushblack, new Rectangle(X - 5, Y , 4, 4));
+            drawingSpace.Graphics.DrawString($"{this}", TextHelpers.drawFont, TextHelpers.writingBrush, X -25, Y - 25);
         }
 
         // De manière textuelle
